d48476c3be829e6955be6322acdc268b45d29eb1
dingtimin <dingtimin@163.com>
2023-05-17 16:31:37 +0800

    fix bug

--- model.py
+++ model.py
@@ -12,8 +12,6 @@ class WhisperModule(Seq2SeqTransformer):
 
     def __init__(self, *args: Any, downstream_model_type=WhisperForConditionalGeneration, lora=False, **kwargs: Any):
         super().__init__(*args,downstream_model_type=downstream_model_type, **kwargs)
-        self.wer = evaluate.load("wer")
-
         self.tokenizer = WhisperTokenizer.from_pretrained("openai/whisper-large-v2", language="chinese", task="transcribe")
         if lora == True:
             config = LoraConfig(r=32, lora_alpha=64, target_modules=["q_proj", "v_proj"], lora_dropout=0.05,
@@ -26,7 +24,7 @@ class WhisperModule(Seq2SeqTransformer):
         tgt_lns = self.tokenize_labels(batch["labels"])
         pred_lns = self.model.generate(inputs=batch["input_features"], language="chinese", task="transcribe")
         # wrap targets in list as score expects a list of potential references
-        result = self.wer(preds=pred_lns, target=tgt_lns)
+        result = 100 * self.wer.compute(preds=pred_lns, target=tgt_lns)
         self.log(f"{prefix}_wer", result, on_step=False, on_epoch=True, prog_bar=True)
 
     def configure_metrics(self, stage: str):
